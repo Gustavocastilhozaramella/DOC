5d7906069d4eb769a7aa28abc5dc11037d4296ea
Gustavocastilhozaramella <38504887+Gustavocastilhozaramella@users.noreply.github.com>
2019-06-04 11:20:29 -0300

    Criando projeto

---
+++ ProgramaGIT.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aruquivo_GITHUB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome = "Gustavo";
+            int idade = 0;
+
+            Console.WriteLine("Digite sua idade: ");
+            idade = Console.Read();
+
+            if (idade > 20 && idade < 40)
+            {
+                Console.WriteLine("Gustavo é Jovem");
+
+            }
+            else {
+                Console.WriteLine("O que é isso?");
+            }
+                Console.ReadKey();
+            
+        }
+    }
+}
